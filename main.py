f6b77631e93cdc89033a439a0422d706795e529c
Olexandr Minin <alminin@Olexandrs-MacBook-Pro.local>
2022-05-03 22:06:59 +0300

    add image processing

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import logging
 from decouple import config
+import os
+from datetime import datetime
 
 from aiogram import Bot, Dispatcher, executor, types
 from PIL import Image, ImageDraw, ImageFont
 
 
-API_TOKEN = config("TOKEN")
+API_TOKEN = config('TOKEN')
+IMAGES_DIR = 'images/'
 
 # Configure logging
 logging.basicConfig(
@@ -21,20 +24,20 @@ def make_caption(filepath, text):
     '''Make caption on received photo'''
     image = Image.open(filepath)
     draw = ImageDraw.Draw(image)
-    font = ImageFont.truetype('Lobster-Regular.ttf', size=40, encoding="utf-8")
+    font = ImageFont.truetype('Lobster-Regular.ttf', size=70, encoding="utf-8")
     width_image, height_image = image.size
     width_text, height_text = draw.textsize(text, font=font)
     draw.text(
-        ((width_image - width_text) / 2 + 1, ((height_image / 10) * 9) + 1),
+        ((width_image - width_text) / 2 + 1, ((height_image / 10) * 8.5) + 5),
         text,
         font=font,
         fill=(0, 0, 0, 0)
     )
     draw.text(
-        ((width_image - width_text) / 2, ((height_image / 10) * 9)),
+        ((width_image - width_text) / 2, ((height_image / 10) * 8.5)),
         text,
         font=font,
-        fill=(255, 0, 0, 0)
+        fill=(255, 255, 255, 0)
     )
     image.save(filepath)
 
@@ -48,9 +51,16 @@ def get_text(user_id, message_id):
             if f'{user_id}-{message_id}' in line:
                 return line.strip('\n').split(':-:')[1]
 
+def get_user_img(user_id):
+    '''
+    Return file path of the last downloaded image of the user
+    '''
+    for file in os.listdir(IMAGES_DIR):
+        if file.startswith(f"{user_id}"):
+            return os.path.join(IMAGES_DIR, file)
 
 async def on_startup(_):
-    print('Bot is omline')
+    print('Bot is online')
 
 @dp.message_handler(commands=['start', 'help'])
 async def command_start(message: types.Message):
@@ -58,16 +68,19 @@ async def command_start(message: types.Message):
     This handler will be called when user sends `/start` or `/help` 
     command
     """
-    await message.reply("Hi!\nLet's make a meme out of your photo!\nSend me a photo, type a text and get your mem image.")
+    await message.reply("Привет!\nДавай сделаем мем из твоего фото!\nВышли мне фото, введи текст для подписи и получи свой мем.")
 
 @dp.message_handler(content_types=['photo'])
-async def ask_caption_text(message: types.Message):
+async def save_image(message: types.Message):
     '''
     This handler will be called when user sends image.
-    Bot asks user to input text for caption.
+    Bot download the image for further processing and asks user to input text for caption.
     '''
+    file_name = f"{message.from_user.id}_{message.message_id}.jpg"
+    file_path = os.path.join(IMAGES_DIR, file_name)
+    await message.photo[-1].download(file_path)
     #await message.reply('What text would you like to add?')
-    await message.answer('What text would you like to add?')
+    await message.answer('Какой текст ты хочешь добавить?')
 
 @dp.message_handler()
 async def change_image(message: types.Message):
@@ -75,17 +88,14 @@ async def change_image(message: types.Message):
     Processing image and send new image to user
     '''
     save_text(message.from_user.id, message.message_id, message.text)
-    await message.answer('Doing my magic!')
-    caption = get_text(message.from_user.id, message.message_id)
-    await message.answer(f'Your caption: {caption}') # debug
-
-# @dp.message_handler()
-# async def echo(message: types.Message):
-#     # old style:
-#     # await bot.send_message(message.chat.id, message.text)
-
-#     await message.answer(message.text)
-
+    await message.answer('Делаю магию!')
+    user_id = message.from_user.id
+    caption = get_text(user_id, message.message_id)
+    img_path = get_user_img(user_id)
+    make_caption(img_path, caption)
+    with open(img_path, 'rb') as f:
+        await message.answer_photo(f)
+    os.remove(img_path)
 
 
 if __name__ == '__main__':
